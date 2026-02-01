46e7bb82e9bedcb9aed7af3bbff4d229ba0f1522
Gabriel Silva <gabriel.fsilva.ext@sodexo.com>
2026-02-01 15:43:44 -0300

    feat: add phone registration endpoint

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import UserAccount
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ("phone_number", "is_active", "created_at")
+    search_fields = ("phone_number",)
