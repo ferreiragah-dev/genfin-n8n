ebb5a8dfadfaf9d3ac5072f698a22d04d0ac2b43
Gabriel Silva <gabriel.fsilva.ext@sodexo.com>
2026-02-07 22:27:00 -0300

    v16

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import UserAccount
 
 @admin.register(UserAccount)
 class UserAccountAdmin(admin.ModelAdmin):
-    list_display = ("phone_number", "is_active", "created_at")
-    search_fields = ("phone_number",)
+    list_display = ("phone_number", "first_name", "last_name", "email", "is_active", "created_at")
+    search_fields = ("phone_number", "first_name", "last_name", "email")
+    list_filter = ("is_active", "created_at")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
+    fieldsets = (
+        ("Conta", {"fields": ("phone_number", "email", "password")}),
+        ("Perfil", {"fields": ("first_name", "last_name")}),
+        ("Status", {"fields": ("is_active", "created_at")}),
+    )
